48ac5f72b6a4ab3c7e16ff10bde1ea157c8b809c
Pedro Tavares <xpedrotavaresdev@gmail.com>
2021-04-02 19:32:15 -0300

    fix footer responsiveness

--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -2,20 +2,60 @@ import styled from 'styled-components'
 
 export const FooterContainer = styled.div`
     display:flex;
+    flex-wrap: wrap;
     justify-content:flex-end;
-    align-items:center;
+    align-items:flex-end;
     background-color: #304251;
     height: 82px;
+p{
+    margin-right:auto;
+    margin-left:auto;
+    justify-self:center;
+    margin-bottom:0;
+    color:white;
 
+
+    font-family: 'Roboto';
+font-size: 15px;
+font-style: normal;
+font-weight: 300;
+letter-spacing: -0.015em;
+text-align:center;
+
+width: 42vw;
+    
+}
+div{
+    display:flex;
+    align-items: center;
+    height:100%;
+    width:100%;
+} 
+
+.logo-casanova-container{
+    display:none;
+}
+.logo-casanova{
+    display:none;
+    width:150px;
+    margin-right:auto;
+    margin-left:auto;
+    margin-bottom:10px
+}
     .back-to-top-desktop{
-        display:flex;
-        position: relative;
+        display:initial;
         width:82px;
+        /* position:fixed;
         right:13vw;
-        bottom: 41px;
-        cursor: pointer;
+        bottom:42px;
+        cursor: pointer; */
 
     }
+    .back-to-top-container{
+            position: relative;
+            bottom:130px;
+            margin-right: 13vw;
+        }
     .back-to-top-mobile{
         display:none;
         width: 54px;
@@ -23,15 +63,34 @@ export const FooterContainer = styled.div`
     }
 
     @media (max-width: 800px){
+        div{
+            height:50%;
+        }
+        .back-to-top-container{
+            position: relative;
+            bottom:175px;
+            margin-right: 5vw;
+        }
 
+        height: 150px;
+        .logo-casanova-container{
+            display:inherit;
+        }
+        .logo-casanova{
+    display:inherit;
+    /* margin-top: 30px; */
+        }
+        p{
+            font-size:12px;
+            width: 300px;
+
+        }
         .back-to-top-desktop{
         display:none
     }
     .back-to-top-mobile{
-        display:flex;
-        position: relative;
-        right:5vw;
-        bottom: 41px;
+        display:inherit;
+       
 
     }
     }
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,12 +7,20 @@ import {FooterContainer} from './FooterElements'
 
 import backtotopMobile from '../../images/backtotop-icon-mobile.png'
 import backtotopDesktop from '../../images/backtotop-icon-desktop.png'
+import casaNovaLogo from '../../images/logo-casanova.png'
 
 const Footer = () => {
     return (
         <FooterContainer>
-            <Link activeClass="active" to="home" spy={true} smooth={true} offset={-10} duration={600}>
+            <div className="logo-casanova-container">
 
+            <img className="logo-casanova" src={casaNovaLogo} />
+            </div>
+        <div>
+
+            <p>@ 2021 CasaNova Design & Build. All rights reserved. Site designed and developed by Contra Fluxo</p>
+        </div>
+            <Link className="back-to-top-container" activeClass="active" to="home" spy={true} smooth={true} offset={-70} duration={600}>
             <img className="back-to-top-desktop" src={backtotopDesktop} alt="back to top icon" />
             <img className="back-to-top-mobile" src={backtotopMobile} alt="back to top icon" />
             </Link>
