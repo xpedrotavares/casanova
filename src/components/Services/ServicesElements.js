2eb73a6abcb88e9e7c4f60012e6e9df78847f4ee
Pedro Tavares <xpedrotavaresdev@gmail.com>
2021-04-08 02:20:57 -0300

    add read more, overflow ...

--- src/components/Services/ServicesElements.js
+++ src/components/Services/ServicesElements.js
@@ -7,7 +7,20 @@ height:40%;
 max-width: 80%;
 justify-content: space-around;
 margin-top: 120px;
+.display-off{
+  display:none;
+}
+.overflow-on{
+overflow: hidden;
+  /* white-space: nowrap; */
+  text-overflow: ellipsis;
+  display: -webkit-box;
+   -webkit-box-orient: vertical;
+   -webkit-line-clamp: 3; /* number of lines to show */
+   line-height: 1.5em;        /* fallback */
+   max-height: 1.5*3;       /* fallback */
 
+}
 @media screen and (max-width:800px){
   margin-top:50px;
 }
@@ -22,6 +35,29 @@ text-align:left;
 padding: 0 40px;
 height:fit-content;
 margin-bottom:30px!important;
+
+button{
+  height:20px;
+  color: #223240;
+  /* background-color: #223140; */
+  font-size:11px;
+  font-weight:500;
+  text-transform: none;
+  border-radius:15px;
+  /* border-color: #223240; */
+  border:0.2px solid #223240; 
+  margin-top:-20px;
+  //F3A98D #223240 #000000 #223140
+}
+
+.MuiTouchRipple-root{
+  /* color: rgb(255,255,255,0.9); */
+
+}
+
+button:hover{
+  /* background-color:#223240; */
+}
 
 @media screen and (max-width:1900px){
     padding:0 70px;
@@ -59,11 +95,13 @@ margin-bottom: 0px;
 `;
 
 export const ServiceParagraph = styled.p`
+
 font-family: "Roboto";
 font-style: normal;
 font-weight: 300;
 font-size: 20px;
 line-height: 30px;
+
 
 @media screen and (max-width: 1900px) {
 
--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import hatIcon from "../../images/hat-icon.svg";
 import coneIcon from "../../images/cone-icon.svg";
 import tapIcon from "../../images/tape-icon.svg";
 import penIcon from "../../images/pen-icon.svg";
+
+import Button from '@material-ui/core/Button'
 
 import {
   
@@ -13,57 +15,76 @@ import {
     ServiceParagraph,
   
   } from "./ServicesElements";
+  
+  
+  const Services = () => {
+    const [overflow, setOverflow] = useState(true);
+    const [display, setDisplay] = useState(true);
+    const handleClick = () =>{
+   setOverflow(false);
+   setDisplay(false);
 
-const Services = () => {
+      
+  }
+
+
+
+  
+ 
+
     return (
       
                <ServicesContainer container>
           <ServiceItem item md={6} xl={3} >
-            <img src={tapIcon} width="70" alt="Tape in orange" />
+            <img src={tapIcon} width="80" alt="Tape in orange" />
 
             <ServicesTitle>Phase 1</ServicesTitle>
-            <ServiceParagraph>
+            <ServiceParagraph className={overflow ? "overflow-on" : ""}>
               In the initial phase we will reach out to better understand your
               vision and expectations. When we come out to assess your project
               we will take measurements to provide you with a free estimate and
               a 3D rendering of what the results will look like.
             </ServiceParagraph>
+            <Button onClick={handleClick} className={display ? "" : "display-off"}  variant="outlined">Read more</Button>
           </ServiceItem>
 
           <ServiceItem item md={6} xl={3}>
             <img src={penIcon} width="80" alt="Pen in orange" />
 
             <ServicesTitle>Phase 2</ServicesTitle>
-            <ServiceParagraph>
+            <ServiceParagraph className={overflow ? "overflow-on" : ""}>
               In this phase you’ll receive the estimate alongside an initial 3D
               rendering based on your vision - this way you can visualize what
               your project will look like when finished.
             </ServiceParagraph>
+            <Button onClick={handleClick} className={display ? "" : "display-off"}  variant="outlined">Read more</Button>
           </ServiceItem>
 
           <ServiceItem item md={6} xl={3} >
             <img src={coneIcon} width="80" alt="cone in orange" />
 
             <ServicesTitle>Phase 3</ServicesTitle>
-            <ServiceParagraph>
+            <ServiceParagraph className={overflow ? "overflow-on" : ""}>
               This is the bureaucratic phase. In this phase we’ll agree and sign
               contractual terms and provide you with a selection sheet of all
               materials that have been previously discussed. We will also make
               any changes to the design where needed and provide a rough project
               schedule. Here we go over the nitty gritty
             </ServiceParagraph>
+            <Button onClick={handleClick} className={display ? "" : "display-off"}  variant="outlined">Read more</Button>
           </ServiceItem>
 
           <ServiceItem item md={6} xl={3} >
             <img src={hatIcon} width="80" alt="Construction hat in orange" />
 
             <ServicesTitle>Phase 4</ServicesTitle>
-            <ServiceParagraph>
+            <ServiceParagraph className={overflow ? "overflow-on" : ""}>
               In this phase the magic happens! Together with our team we will
               bring your project to life/make your remodeling wishes come to
               life/come true, always maintaining an open line of communication
               and regular updates throughout the process.
             </ServiceParagraph>
+            <Button onClick={handleClick} className={display ? "" : "display-off"}  variant="outlined">Read more</Button>
           </ServiceItem>
 
         </ServicesContainer>
